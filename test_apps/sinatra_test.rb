808fa2deeb2b525cf3e7f9a67a3ab507576cd838
Aaron Gough <aaron.gough@gmail.com>
2017-04-30 21:46:55 -0400

    Add test sinatra app

---
+++ test_apps/sinatra_test.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+require "bundler/setup"
+require "depthcharge"
+require "sinatra/base"
+require "json"
+
+class SinartraTestApp < Sinatra::Base
+  use Depthcharge::Middleware
+
+  get "/example.json" do
+    content_type :json
+    {a: 1, b: 2, c: 3, d: 4}.to_json
+  end
+
+  run!
+end
